1d095c029194b7e6622b5c6501f2541e057b8629
mikupacchogu <li727167zhang5235@163.com>
2024-07-08 20:17:58 +0800

    基本的な背景・色設定を行えるプログラムを記述しました

--- content_scripts.js
+++ content_scripts.js
@@ -1,8 +1,6 @@
 /// <reference path="lib/jquery.d.ts" />
-
 // content_scripts.js内部のscriptを記述
-
-$(document).ready(function (): void {
+$(document).ready(function () {
     console.log("変更を加えています");
     // 背景画像の設定
     $("body").css({
@@ -12,9 +10,8 @@ $(document).ready(function (): void {
         "background-attachment": "fixed",
         "background-size": "cover",
     });
-
     // 各メニュー欄の背景色設定
     $(".l__item, .group, .m__header, .m__footer").css({
         "background-color": "#ffffffe0"
-    })
+    });
 });
