e99d7251120b8a8d1173e3bddd87e0c2cc2d5786
mikupacchogu <li727167zhang5235@163.com>
2025-01-14 15:01:58 +0800

    [Fix]Modified the description of jquery-dependent processes to work with vanilla Javascript, and removed unused TS files, Jquery files themselves, and imports.

--- content_scripts/content_scripts.js
+++ content_scripts/content_scripts.js
@@ -1,31 +1,24 @@
-/// <reference path="../lib/jquery.d.ts" />
-// content_scripts.js内部のscriptを記述
 function changeBackgroundImage(url) {
     console.log("変更を加えています\n画像URL");
     console.log(url);
     // 背景画像の設定
-    $("body").css({
-        "background-image": "url(".concat(url, ")"),
-        "background-position": "center center",
-        "background-repeat": "no-repeat",
-        "background-attachment": "fixed",
-        "background-size": "cover",
-    });
+    document.body.style.backgroundImage = "url('" + url + "')";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundSize = "cover";
     // 各メニュー欄の背景色設定
-    $(".l__item, .group, .m__header, .m__footer").css({
-        "background-color": "#ffffffe0"
-    });
+    let elements = document.querySelectorAll(".l__item, .group, .m__header, .m__footer");
+    elements.forEach((element => element.style.backgroundColor = "#ffffffe0"));
     console.log("変更が終了しました");
 }
 function load2Call() {
     chrome.storage.sync.get("img_url", function (data) {
-        var url = data["img_url"];
+        let url = data["img_url"];
         changeBackgroundImage(url);
     });
 }
-$(document).ready(function () {
-    load2Call();
-});
+window.addEventListener('load', load2Call);
 chrome.storage.onChanged.addListener(function () {
     load2Call();
 });
--- popup/js/script.js
+++ popup/js/script.js
@@ -1,22 +1,26 @@
-/// <reference path="../../lib/jquery.d.ts" />
-// リアルタイムでURLのプレビューを表示する
-$(function () {
-    $(document).on('input', '#img_url', function (e) {
-        if ($('#img_url').val().bytes() < 8000) {
-            $('#output').text("\u53C2\u7167\u5148\u306E\u30D7\u30EC\u30D3\u30E5\u30FC");
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('img_url').addEventListener('input', function(e) {
+        if (this.value.bytes() <= 8000) {
+            document.getElementById('output').textContent = "参照先のプレビュー";
+        } else {
+            document.getElementById('output').textContent = "URLが長すぎます(8KB以下の容量推奨:".concat(this.value.bytes(), "B)");
         }
-        else {
-            $('#output').text("URL\u304C\u9577\u3059\u304E\u307E\u3059(8KB\u4EE5\u4E0B\u306E\u307F:\u73FE\u5728\u306E\u5BB9\u91CF\u7D04".concat($('#img_url').val().bytes(), "B)"));
-        }
-        $('#output2').wrapInner("<img alt=\"\u30D7\u30EC\u30D3\u30E5\u30FC\" src=\"".concat($('#img_url').val(), "\" width=\"240px\">"));
+        let img = document.createElement('img');
+        img.setAttribute('alt', 'プレビュー');
+        img.setAttribute('src', this.value);
+        img.setAttribute('width', '240px');
+        document.getElementById('output2').innerHTML = '';
+        document.getElementById('output2').appendChild(img);
     });
+
+    document.getElementById('send').addEventListener('click', function() {
+        if (document.getElementById('img_url').value.bytes() <= 8000) {
+            chrome.storage.sync.set({ "img_url": document.getElementById('img_url').value });
+            window.close();
+        }
+    });
+
+    String.prototype.bytes = function() {
+        return (encodeURIComponent(this).replace(/%../g, "x").length);
+    };
 });
-$('#send').on('click', function () {
-    if ($('#img_url').val().bytes() <= 8000) {
-        chrome.storage.sync.set({ "img_url": $('#img_url').val() });
-        window.close();
-    }
-});
-String.prototype.bytes = function () {
-    return (encodeURIComponent(this).replace(/%../g, "x").length);
-};
