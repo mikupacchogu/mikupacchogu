1fbf0bbc97674e18ad9e150ff40fa487776aa9d5
mikupacchogu <li727167zhang5235@163.com>
2025-03-30 13:03:59 +0800

    test:content_scripts.tsのコンパイル後の検証のためcontent_scripts.jsの内容をコンパイルされた内容に変更する

--- content_scripts/content_scripts.js
+++ content_scripts/content_scripts.js
@@ -1,20 +1,21 @@
 function changeBackgroundImage(url) {
-    console.log("変更を加えています\n画像URL");
+    console.log("変更を加えています\n画像URL: ");
     console.log(url);
     // 背景画像の設定
-    document.body.style.backgroundImage = "url('" + url + "')";
-    document.body.style.backgroundPosition = "center center";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundAttachment = "fixed";
-    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundImage = 'url("' + url + '")'; // 背景画像URL
+    document.body.style.backgroundPosition = 'center center'; // 背景位置
+    document.body.style.backgroundRepeat = 'no-repeat'; // 背景の繰り返し
+    document.body.style.backgroundAttachment = "fixed"; // 背景がスクロールで動くか(?)
+    document.body.style.backgroundSize = "cover"; // 背景サイズ
     // 各メニュー欄の背景色設定
-    let elements = document.querySelectorAll(".l__item, .group, .m__header, .m__footer");
-    elements.forEach((element => element.style.backgroundColor = "#ffffffe0"));
+    var elements = document.querySelectorAll(".l__item, .group, .m__header, .m__footer");
+    elements.forEach((function (element) { return element.style.backgroundColor = "#ffffffe0"; }));
     console.log("変更が終了しました");
 }
 function load2Call() {
+    // chromeの同期ストレージからデータを取得し、chengeBackgroundImageへ画像URLを渡す
     chrome.storage.sync.get("img_url", function (data) {
-        let url = data["img_url"];
+        var url = data["img_url"];
         changeBackgroundImage(url);
     });
 }
