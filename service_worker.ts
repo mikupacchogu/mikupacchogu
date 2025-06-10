7682949930d41b29d9dd9066e998da8e2fed7394
mikupacchogu <li727167zhang5235@163.com>
2025-06-11 01:07:19 +0800

    ref:service_worker.tsのコードをリファクタリング(letをconstに置き換え,ネストの簡略化)する

--- service_worker.ts
+++ service_worker.ts
@@ -1,17 +1,20 @@
-chrome.runtime.onInstalled.addListener(function (details: object): function {
+chrome.runtime.onInstalled.addListener(function (details) {
     // インストール時の挙動
     if (details.reason === "install") {
-
-        let defaultSettings = {
-            "img_url": ""
-        };
-
-        chrome.storage.sync.set(defaultSettings, function () {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-            } else {
-                console.log("Initial settings set.");
-            }
-        });
+        syncStorageInit()
     }
 });
+
+function syncStorageInit() {
+    const defaultSettings = {
+        "img_url": ""
+    };
+
+    chrome.storage.sync.set(defaultSettings, function () {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+        } else {
+            console.log("Initial settings set.");
+        }
+    });
+}
