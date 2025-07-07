39e3ce47080d93a2a7d8b1185dbbed212507692f
mikupacchogu <li727167zhang5235@163.com>
2025-07-07 08:40:39 +0800

    change:service_worker.tsのリファクタリングによるservice_worker.jsのコード変更

--- service_worker.js
+++ service_worker.js
@@ -1,16 +1,22 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     // インストール時の挙動
     if (details.reason === "install") {
-        var defaultSettings = {
-            "img_url": ""
-        };
-        chrome.storage.sync.set(defaultSettings, function () {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-            }
-            else {
-                console.log("Initial settings set.");
-            }
-        });
+        syncStorageInit();
     }
 });
+/**
+ * chromにある拡張機能用同期ストレージを初期化する
+ */
+function syncStorageInit() {
+    var defaultSettings = {
+        "img_url": ""
+    };
+    chrome.storage.sync.set(defaultSettings, function () {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+        }
+        else {
+            console.log("Initial settings set.");
+        }
+    });
+}
