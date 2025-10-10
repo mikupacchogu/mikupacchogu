4f4c5d56324afa4b5dc4784f1aee5b2b4e62f18b
mikupacchogu <li727167zhang5235@163.com>
2025-10-10 19:27:19 +0800

    change:script.tsの変更に伴うコンパイル結果をscript.jsに反映させる

--- popup/js/script.js
+++ popup/js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 const BYTE_LIMIT = 8000;
 document.addEventListener('DOMContentLoaded', function () {
+    // 最初にpopupを表示するときに現在設定されている画像をプレビュー表示する
+    chrome.storage.sync.get("img_url", function (data) {
+        const url = data["img_url"];
+        if (url === "") {
+            return;
+        }
+        setPreviewImage("現在設定中の画像", url);
+    });
     // 画像URL入力受付～プレビューまでを行う
     document.getElementById('img_url').addEventListener('input', function (e) {
         const url = this.value;
@@ -17,21 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById("send").style.cssText = "cursor: not-allowed";
             return;
         }
-        let imgElement = new Image();
-        imgElement.alt = 'プレビュー';
-        imgElement.width = 240;
-        imgElement.onload = function () {
-            document.getElementById('message').textContent = "参照先のプレビュー";
-            document.getElementById('img_sample').innerHTML = '';
-            document.getElementById('img_sample').appendChild(imgElement);
-            document.getElementById("send").style.cssText = "";
-        };
-        imgElement.onerror = function () {
-            document.getElementById('message').textContent = "画像が存在しません";
-            document.getElementById('img_sample').innerHTML = '';
-            document.getElementById("send").style.cssText = "cursor: not-allowed";
-        };
-        imgElement.src = url; // URLの設定を忘れずに追加
+        setPreviewImage("参照先のプレビュー", url);
     });
     document.getElementById('send').addEventListener('click', async function () {
         const url = document.getElementById('img_url').value;
@@ -66,10 +60,34 @@ document.addEventListener('DOMContentLoaded', function () {
             img.src = url;
         });
     }
+    /**
+     * プレビュー画像を設定します
+     * @param {string} message 画像の上に表示するメッセージ
+     * @param {string} url 画像URL
+     */
+    function setPreviewImage(message, url) {
+        let imgElement = new Image();
+        imgElement.alt = 'プレビュー';
+        imgElement.width = 240;
+        imgElement.onload = function () {
+            document.getElementById('message').textContent = message;
+            document.getElementById('img_sample').innerHTML = '';
+            document.getElementById('img_sample').appendChild(imgElement);
+            document.getElementById("send").style.cssText = "";
+        };
+        imgElement.onerror = function () {
+            document.getElementById('message').textContent = "画像が存在しません";
+            document.getElementById('img_sample').innerHTML = '';
+            document.getElementById("send").style.cssText = "cursor: not-allowed";
+        };
+        imgElement.src = url; // URLの設定を忘れずに追加
+    }
+    ;
     // 文字列のバイト数を返すプロパティを作成
     String.prototype.bytes = function () {
         return (encodeURIComponent(this).replace(/%../g, "x").length);
     };
+    // 文字列のバイト数が与えられたリミットを超えているか判定するプロパティを作成
     String.prototype.isOverByteLimit = function (byteLimit) {
         return this.bytes() > byteLimit;
     };
